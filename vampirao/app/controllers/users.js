c84895697acf72b5ea01be03f7c37b9508f538a0
Thiago <tbgdmelo@gmail.com>
2021-09-16 17:29:25 -0400

    nova pagina home

--- vampirao/app/controllers/users.js
+++ vampirao/app/controllers/users.js
@@ -1,7 +1,12 @@
 //controlador para todas as páginas referentes ao usuario
 
-function cadastro(req,res){
+const models = require("../models/index"); //import de todos os models
+const Sangue = models.Sangue; //utilizando apenas o model de sangue para exibir nos radios button
+
+async function cadastro(req,res){
+    const sangues = await Sangue.findAll(); //recupera todos os registros da tabela sangues
     res.render("user/cadastro", {
+        sangues: sangues.map(sangue=>sangue.toJSON()), //envia para a view um json com oq foi encontrado no bd
         titulo: "Cadastro de usuário",
     })
 }
