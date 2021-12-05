593bff9107fa2a8992eaed5f91f3f1266b3de1b2
Thiago Melo <tbgdmelo@gmail.com>
2021-12-04 20:15:19 -0400

    seeder de usuarios

--- vampirao/database/seeders/20211005155407-User.js
+++ vampirao/database/seeders/20211005155407-User.js
@@ -20,6 +20,54 @@ module.exports = {
         nivel: "Administrador",
         centro: "Administrador"
       },
+      {
+        cpf: "894.891.540-16",
+        nome:"Admin",
+        sobrenome: "Hemoam",
+        senha: await brcypt.hash("adminhemoam",10),
+        email: "adminhemoam@gmail.com",
+        pontuacao: 0,
+        telefone: "",
+        nascimento: "2021-01-10",
+        id_sangue: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        isAdmin: true,
+        nivel: "Administrador",
+        centro: "Hemoam - Fundação Hospitalar de Hematologia e Hemoterapia do Amazonas"
+      },
+      {
+        cpf: "456.281.590-69",
+        nome:"Admin",
+        sobrenome: "Doe Vida",
+        senha: await brcypt.hash("admindoevida",10),
+        email: "admindoevida@gmail.com",
+        pontuacao: 0,
+        telefone: "",
+        nascimento: "2021-01-10",
+        id_sangue: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        isAdmin: true,
+        nivel: "Administrador",
+        centro: "DOE VIDA"
+      },
+      {
+        cpf: "967.718.180-71",
+        nome:"Admin",
+        sobrenome: "Hemoam São José I",
+        senha: await brcypt.hash("adminsj1",10),
+        email: "adminhemoamsj1@gmail.com",
+        pontuacao: 0,
+        telefone: "",
+        nascimento: "2021-01-10",
+        id_sangue: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        isAdmin: true,
+        nivel: "Administrador",
+        centro: "HEMOAM-São Jose I"
+      }
       ], {});
   },
 
